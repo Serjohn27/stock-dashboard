29931105a137db465888c20299b4f89bbd18144f
unknown <serjohn2727@gmail.com>
2019-10-03 22:12:21 -0400

    added new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PriceQuoteComponent } from './price-quote/price-quote.component';
+import { CandlestickChartComponent } from './candlestick-chart/candlestick-chart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PriceQuoteComponent
+    PriceQuoteComponent,
+    CandlestickChartComponent
   ],
   imports: [
     BrowserModule,
