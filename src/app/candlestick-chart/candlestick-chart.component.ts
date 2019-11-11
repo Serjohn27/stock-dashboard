9420fb2de42bf1342eed82f62a341436ec7c8f8c
unknown <serjohn2727@gmail.com>
2019-11-10 21:15:31 -0500

    fixed chart

--- src/app/candlestick-chart/candlestick-chart.component.ts
+++ src/app/candlestick-chart/candlestick-chart.component.ts
@@ -29,71 +29,63 @@ export class CandlestickChartComponent implements OnInit {
 
   drawChart(priceQuote: PriceQuote) {
 
-      const margin = {top: 20, right: 10, bottom: 20, left: 10};
-      const width = 720 - margin.left - margin.right;
-      const height = 400 - margin.top - margin.bottom;
+      const margin = {top: 30, right: 70, bottom: 30, left: 70};
 
-      const svg = d3.select('#candlestick-chart-container')
-          .append('svg')
-          .attr('width', width + margin.left + margin.right)
-          .attr('height',height + margin.top + margin.bottom)
+      const width = 720;
+      const height = 400;
+
+      const innerWidth = width - margin.left - margin.right;
+      const innerHeight = height - margin.top - margin.bottom;
+
+      let candles = priceQuote.candles;
+
+      var chart = d3.select("#candlestick-chart-container")
+        .append("svg")
+          .attr("width", width)
+          .attr("height", height)
           .style('background-color','#9cb5aa')
           .append('g')
-          .attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+          .attr('transform', `translate(${margin.left}, ${margin.right})`);
 
-      let candles = priceQuote.candles; 
-      //Map dates from candles
-      const dates = candles.map( candle => new Date(candle.datetime));
-      const xmin = d3.min(dates);
-      const xmax = d3.max(dates);
-      //Date Scale    
-      const xScale = d3.scaleTime().domain([xmin,xmax]).range([0, width])
-      const xAxis = d3.axisBottom().scale(xScale);
 
-      const gX = svg.append("g")
-					.attr("class", "axis x-axis") //Assign "axis" class
-					.attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      
+      //  Y Axis with Price          
       const prices = candles.map(candle => candle.close);
       const ymin = d3.min(prices);
-      const ymax = d3.max(prices);
-      const yScale = d3.scaleLinear().domain([ymin,ymax]).range(0,height);
-      const yAxis = d3.axisLeft().scale(yScale);
+      const ymax = d3.max(prices);    
 
-      const gy = svg.append("g")
-					.attr("class", "axis y-axis") //Assign "axis" class
-          .call(yAxis);
+      // Create the scale
+      const yScale = d3.scaleLinear()
+          .domain([ymin, ymax])       
+          .range([innerHeight, 0]);  
+
+      const yAxis = d3.axisLeft(yScale);    
+
+      chart
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+      .call(yAxis);
+
+      
+      
+      // X Axis with Dates
+
+      // Map dates from candles
+       const dates = candles.map( candle => new Date(candle.datetime));
+       const xmin = d3.min(dates);
+       const xmax = d3.max(dates);
+      // Date Scale    
+      const xScale = d3.scaleTime()
+                         .domain([xmin,xmax])
+                         .range([0, innerWidth]);
 
 
-      //   const xScale = d3.scaleTime().domain([xmin,xmax]).range(0,width);
-      //  console.log(xScale(xmin));
+       const xAxis = d3.axisBottom().scale(xScale);
 
-
-
-
-         // const xScale = d3.scaleLinear().domain([xmin,xmax]).range(0,width);
-
-       //   const xDateScale = d3.scaleQuantize().domain([0, dates.length]).range(dates);
-      //    let xBand = d3.scaleBand().domain(d3.range(xmin,xmax)).range([0, width]).padding(0.3);
-
-
-
-           console.log('date' + dates[0])
-       //    console.log(xBand(dates[0]))
-     //     const xAxis = d3.axisBottom().scale(xBand);
-       
-        
-  
-          // const gX = svg.append("g")
-					// .attr("class", "axis x-axis") //Assign "axis" class
-					// .attr("transform", "translate(0," + height + ")")
-          // .call(xAxis);
-
-   
+       const gX = chart.append("g")
+                 .attr("class", "axis x-axis") 
+                 .attr('transform', `translate(0, ${innerHeight})`)
+                 .call(xAxis);
           
-    
+
   }
 
   
