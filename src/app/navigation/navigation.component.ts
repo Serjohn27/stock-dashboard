f55d9e7e67b7cf92e00d635574fab6393cb27583
Sercan <serjohn2727@gmail.com>
2020-01-01 23:49:08 -0500

    added nav menu with toggle

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-  constructor() { }
+   navigationActive: boolean;
+
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.dataService.currentState.subscribe(message => this.navigationActive = message);
   }
 
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -9,9 +9,22 @@ export class DataService {
   private messageSource = new BehaviorSubject<string>(null);
   currentMessage = this.messageSource.asObservable();
 
+
+  private navigationToggle = new BehaviorSubject<boolean>(false);
+  currentState = this.navigationToggle.asObservable();
+
   constructor() { }
 
   sendMessage(message: string) {
       this.messageSource.next(message);
+  }
+
+  toggleNavigationMenu() {
+      if(this.navigationToggle.getValue()==false){
+          this.navigationToggle.next(true);
+      }
+      else{
+        this.navigationToggle.next(false);
+      }
   }
 }
